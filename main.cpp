ee338ecb524de9f82c5b434776477a47766f5dd3
scarf <greenscarf005@gmail.com>
2022-06-02 22:39:20 +0900

    build:  더미 메인 파일 추가

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  bool flag = true;
+
+  if (flag) {
+    std::cout << "true" << std::endl;
+  } else {
+    std::cout << "false" << std::endl;
+  }
+}
--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,11 @@
 #include <algorithm>
 #include <vector>
 
-int main() {
+#if defined(TEST)
+# define TEST_MAIN main
+#endif
+
+int TEST_MAIN() {
   rc::check("double reversal yields the original value",
             [](const std::vector<int> &l0) {
               auto l1 = l0;
@@ -11,6 +15,8 @@ int main() {
               std::reverse(begin(l1), end(l1));
               RC_ASSERT(l0 == l1);
             });
+
+  int flag = 0;
 
   return 0;
 }
