27b97cee29e569626329e03255392529959fbe52
scarf <greenscarf005@gmail.com>
2022-06-02 22:53:16 +0900

    build: googletest 추가

---
+++ dummyfunc.cpp
@@ -0,0 +1,2 @@
+int addi(int a, int b) { return a + b; }
+float addf(float a, float b) { return a + b; }
--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,26 @@
+#include <gtest/gtest.h>
 #include <rapidcheck.h>
 
 #include <algorithm>
 #include <vector>
 
-#if defined(TEST)
-# define TEST_MAIN main
-#endif
+// function to test
+int addi(int a, int b);
 
-int TEST_MAIN() {
+// not covered by tests
+float addf(float a, float b);
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+
+  EXPECT_EQ(addi(1, 2), 3);
+  // Should fail
+//   EXPECT_NE(7 * 6, 42);
+
   rc::check("double reversal yields the original value",
             [](const std::vector<int> &l0) {
               auto l1 = l0;
@@ -15,8 +28,6 @@ int TEST_MAIN() {
               std::reverse(begin(l1), end(l1));
               RC_ASSERT(l0 == l1);
             });
-
-  int flag = 0;
-
-  return 0;
 }
+
+// TODO: use RC_GTEST_PROP
