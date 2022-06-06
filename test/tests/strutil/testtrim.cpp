5c9004eb435335a22dcd9d3f979cb98c66fff49b
scarf <greenscarf005@gmail.com>
2022-06-06 19:32:58 +0900

    test: erase_* 테스트 추가

--- test/tests/strutil/testtrim.cpp
+++ test/tests/strutil/testtrim.cpp
@@ -10,20 +10,22 @@
 
 using strvec = std::vector<std::string>;
 
-RC_GTEST_PROP(TrimTest, general, (const std::string &s)) {
-  static const std::array whitespaces{" ", "\t", "\n", "\r", "\f", "\v"};
+static const std::array whitespaces{" ", "\t", "\n", "\r", "\f", "\v"};
 
+auto gen_whitespace(size_t max_length = 10) -> std::string {
+  size_t length = *rc::gen::inRange(0ul, max_length);
+
+  std::string res;
+  for (size_t i = 0; i < length; ++i)
+    res += *rc::gen::elementOf(whitespaces);
+  return res;
+};
+
+RC_GTEST_PROP(TrimTest, general, (const std::string &s)) {
   RC_PRE(std::none_of(
       whitespaces.begin(), whitespaces.end(),
       [&](const std::string &ws) { return s.find(ws) != std::string::npos; }));
-  const auto gen_whitespace = [&](size_t max_length = 100) {
-    size_t length = *rc::gen::inRange(0ul, max_length);
 
-    std::string res;
-    for (size_t i = 0; i < length; ++i)
-      res += *rc::gen::elementOf(whitespaces);
-    return res;
-  };
   const std::string lws = gen_whitespace(), rws = gen_whitespace();
   const auto gen_test_strings = [&]() {
     return std::array<std::string, 4>{lws + s, s + rws, lws + s + rws, s};
@@ -53,6 +55,16 @@ RC_GTEST_PROP(TrimTest, empty, (const std::string &delim)) {
   util::trim_func funcs[3] = {util::ltrim, util::rtrim, util::trim};
   for (auto func : funcs) {
     std::string s = "";
-    ASSERT_EQ(func(s, delim), "");
+    RC_ASSERT(func(s, delim) == "");
   }
+}
+
+RC_GTEST_PROP(eraseTest, general,
+              (const std::string &s, const std::string &f)) {
+  RC_PRE(!f.empty() && !s.empty() && s.find(f) == std::string::npos);
+
+  std::string tmp1 = s + f, tmp2 = f + s;
+
+  RC_ASSERT(util::erase_from(tmp1, f) == s);
+  RC_ASSERT(util::erase_before(tmp2, f) == s);
 }
