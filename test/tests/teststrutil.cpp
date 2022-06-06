1dcdcfd9fcb5a9ac085c69c261f39d8cd1717584
scarf <greenscarf005@gmail.com>
2022-06-06 15:56:53 +0900

    test: join 테스트 추가

--- test/tests/teststrutil.cpp
+++ test/tests/teststrutil.cpp
@@ -8,6 +8,24 @@
 
 using strvec = std::vector<std::string>;
 
+RC_GTEST_PROP(JoinTest, general, (const strvec &v, const std::string &delim)) {
+  RC_PRE(!v.empty());
+  const size_t expected_length =
+      std::max(v.size() - 1, 0UL) * delim.length() +
+      std::accumulate(
+          v.begin(), v.end(), 0,
+          [](size_t acc, const std::string &s) { return acc + s.size(); });
+
+  const std::string joined = util::join(v, delim);
+  RC_ASSERT(joined.length() == expected_length);
+}
+
+RC_GTEST_PROP(JoinTest, empty, (const std::string &delim)) {
+  strvec v = {};
+  const std::string joined = util::join(v, delim);
+  RC_ASSERT(joined.empty());
+}
+
 RC_GTEST_PROP(SplitTest, general, (const strvec &v, const std::string &delim)) {
   RC_PRE(!delim.empty());
   RC_PRE(std::none_of(v.begin(), v.end(), [&](const std::string &s) {
