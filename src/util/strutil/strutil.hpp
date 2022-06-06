fd63cb191a67533f76cfb3b5138209d30b037dea
scarf <greenscarf005@gmail.com>
2022-06-06 18:51:53 +0900

    refactor: 정리 및 문서 추가

--- src/util/strutil/strutil.hpp
+++ src/util/strutil/strutil.hpp
@@ -19,9 +19,11 @@ string& erase_from(string& line, const string& match);
 string& ltrim(string& s, const string& match = white_space);
 string& rtrim(string& s, const string& match = white_space);
 string& trim(string& s, const string& match = white_space);
+typedef string& (*trim_func)(string& s, const string& match);
 
 vector<string> split(const string& str, const string& sep = "");
 
+/// stringstream 출력을 지원하는 원소에 한해 문자열로 변경
 template <typename T>
 string to_string(const T& t) {
   std::stringstream ss;
@@ -30,18 +32,16 @@ string to_string(const T& t) {
   return ss.str();
 }
 
+/**
+ * @brief 파이썬의 str.join(이터러블)과 유사한 기능을 제공하는 함수
+ *
+ * @tparam Container 이터레이터의 원소가 << 출력을 지원하는 컨테이너
+ * @param sep 연결 문자열, 기본값 ""
+ */
 template <typename Container>
-string join(const Container& c, const string& sep = "") {
-  std::stringstream ss;
+string join(const Container& c, const string& sep = "");
 
-  for (size_t i = 0; i < c.size(); ++i) {
-    if (i != 0)
-      ss << sep;
-    ss << c[i];
-  }
-  return ss.str();
-}
-
+/// @deprecated prettyprint()로 대신하기
 template <typename T>
 void print_vector(vector<T> v, bool oneline = true) {
   const string sep = oneline ? ", " : "\n";
@@ -52,5 +52,7 @@ void print_vector(vector<T> v, bool oneline = true) {
 }
 
 }  // namespace util
+
+#include "strutil.tpp"
 
 #endif  // STRUTIL_STRUTIL_HPP
--- src/util/strutil/trim.cpp
+++ src/util/strutil/trim.cpp
@@ -3,28 +3,45 @@
 namespace util {
 const string white_space = " \t\n\r\f\v";
 
-// trim from end of string (right)
+/**
+ * @brief 문자열 오른쪽 지우기
+ *
+ * @param match 해당하지 않는 문자열 바로 뒤를 지움
+ */
 string& rtrim(string& s, const string& match) {
   s.erase(s.find_last_not_of(match) + 1);
   return s;
 }
 
-// trim from beginning of string (left)
+/**
+ * @brief 문자열 왼쪽 지우기
+ *
+ * @param match 해당하지 않는 문자열 바로 앞을 지움
+ */
 string& ltrim(string& s, const string& match) {
   s.erase(0, s.find_first_not_of(match));
   return s;
 }
 
-// trim from both ends of string (right then left)
+/**
+ * @brief 문자열 양쪽 지우기
+ *
+ * @param match 해당하지 않는 문자열 전후를 지움
+ */
 string& trim(string& s, const string& match) {
   return ltrim(rtrim(s, match), match);
 }
 
+/**
+ * @deprecated 이거 rtrim으로 안댐?
+ *
+ * @param match 해당하지 않는 문자열 바로 뒤를 지움
+ */
 string& erase_from(string& line, const string& match) {
-  string::size_type comment_index = line.find(match);
+  string::size_type index = line.find(match);
 
-  if (comment_index != string::npos)
-    line.erase(comment_index);
+  if (index != string::npos)
+    line.erase(index);
   return line;
 }
 }  // namespace util
