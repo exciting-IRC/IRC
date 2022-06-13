ed8624f771eb8f420b0012b7e9e028d416344679
cjeon <cjeon@student.42seoul.kr>
2022-06-13 20:27:19 +0900

    메인 이벤트루프

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,42 @@
+#include <signal.h>
+#include <string.h>
 #include <iostream>
-#include <util/strutil/strutil.hpp>
-#include <vector>
-#include <string>
+
+#include "event/event.hpp"
+#include "network/socket/socket.hpp"
+#include "util/FixedBuffer/FixedBuffer.hpp"
+#include "Client.hpp"
+
+const static char *bind_addr = "127.0.0.1";
+const static int port = 7778;
+
+static sig_atomic_t recived_sig;
+
+void server_close_handler(int sig) { recived_sig = sig; }
 
 int main() {
-  using namespace std;
-  vector<string> v = util::split("a,b,c", ",");
-  util::print_vector(v);
+  signal(SIGINT, server_close_handler);
+  signal(SIGTERM, server_close_handler);
+  signal(SIGQUIT, server_close_handler);
+
+  Server srv(bind_addr, port, 64);
+  if (!srv.ok())
+    err(1, "server_init");
+
+  std::cout << "Listen at " << bind_addr << ":" << port << std::endl;
+
+  EventPool pool(64);
+  if (!pool.ok())
+    err(1, "event_pool_init");
+
+  pool.addEvent(kRead, &srv);
+
+  while (true) {
+    int k = pool.dispatchEvent(1);
+    if (recived_sig) {
+      printf("shutdown... %s\n", strsignal(recived_sig));
+      return 0;
+    }
+  }
+  return 0;
 }
