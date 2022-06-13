6686b072249cf0e338776c87a928713c60a7769d
scarf <greenscarf005@gmail.com>
2022-06-13 21:09:21 +0900

    fix: 종속성 추가

--- cpputil.py
+++ cpputil.py
@@ -1,5 +1,7 @@
 from subprocess import PIPE, run
 
+from docopt import docopt
+
 
 def clang_format(text: str) -> str:
     result = run(["clang-format"], stdout=PIPE, input=text, encoding="ascii")
@@ -7,8 +9,8 @@ def clang_format(text: str) -> str:
         raise Exception(str(result))
     return result.stdout
 
+
 def run_docopt(doc: str | None) -> dict[str, str]:
-    from docopt import docopt
 
     assert doc is not None and doc != __doc__
     return docopt(doc)
