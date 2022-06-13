dde6df2e8b2ef69c40191efe1919276e02b05b7b
cjeon <cjeon@student.42seoul.kr>
2022-06-13 20:26:17 +0900

    에코서버 서버 - 클라이언트 이벤트 핸들러 클래스 추가

---
+++ src/Server.hpp
@@ -0,0 +1,38 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <list>
+#include "event/event.hpp"
+#include "util/FixedBuffer/FixedBuffer.hpp"
+
+class Server;
+class Client;
+
+typedef std::list<Client *> ClientList;
+typedef util::FixedBuffer<char, 512> Buffer;
+
+class Server : public EventHandler {
+ public:
+  Server(const char *listen_addr, int port, int backlog);
+  ~Server();
+
+ private:
+  Server(const Server &);
+  Server &operator=(const Server &);
+
+ public:
+  bool ok();
+
+  int getFd() const;  // override, final
+
+  int handle(Event e);  // override, final
+
+  void removeClient(ClientList::iterator pos);
+
+ private:
+  ClientList client_list_;
+  int sock_;
+  bool ok_;
+};
+
+#endif
