6e411c7cbda2a13cce29743c7352155bafac283a
scarf <greenscarf005@gmail.com>
2022-06-14 20:11:10 +0900

    refactor: 추후 리팩터링을 위해 구조체에 포매팅 상태 저장

--- src/util/strutil/format.cpp
+++ src/util/strutil/format.cpp
@@ -5,45 +5,50 @@ namespace util {
 using std::string;
 using std::vector;
 
-string format(const string& fmt, const vector<string>& args) {
-  int arg_counter = 0;
+struct FormatState {
+  string fmt;
+  vector<string> args;
   std::string result;
+  int arg_counter;
+
+  FormatState(const string& fmt, const vector<string>& args)
+      : fmt(fmt), args(args), arg_counter(0) {}
+};
+
+string format(const string& fmt, const vector<string>& args) {
+  FormatState state(fmt, args);
 
   for (size_t i = 0; i < fmt.size(); i++) {
-    if (fmt[i] == '{') {
-      if (fmt[i + 1] == '{') {
-        result += '{';
-        i++;
-      } else {
-        result += args.at(arg_counter++);
-        size_t j = i + 1;
-        for (; j < fmt.size(); j++) {
-          if (fmt[j] == '{') {
-            throw std::runtime_error("unmatched opening brace at " +
-                                     util::to_string(j));
-          } else if (fmt[j] == '}') {
-            i = j;
-            break;
-          }
-        }
-        if (j == fmt.size()) {
-          throw std::runtime_error(
-              "missing closing brace '}' at end of format string");
+    if (fmt.substr(i, i + 1) == "{{") {
+      state.result += '{';
+      i++;
+    } else if (fmt[i] == '{') {
+      state.result += args.at(state.arg_counter++);
+      size_t j = i + 1;
+      for (; j < fmt.size(); j++) {
+        if (fmt[j] == '{') {
+          throw std::runtime_error("unmatched opening brace at " +
+                                   util::to_string(j));
+        } else if (fmt[j] == '}') {
+          i = j;
+          break;
         }
       }
+      if (j == fmt.size()) {
+        throw std::runtime_error(
+            "missing closing brace '}' at end of format string");
+      }
+    } else if (fmt.substr(i, i + 1) == "}}") {
+      state.result += '}';
+      i++;
     } else if (fmt[i] == '}') {
-      if (fmt[i + 1] == '}') {
-        result += '}';
-        i++;
-      } else {
-        throw std::runtime_error("unmatched closing brace at " +
-                                 util::to_string(i));
-      }
+      throw std::runtime_error("unmatched closing brace at " +
+                               util::to_string(i));
     } else {
-      result += fmt[i];
+      state.result += fmt[i];
     }
   }
-  return result;
+  return state.result;
 }
 
 }  // namespace util
