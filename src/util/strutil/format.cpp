57bbb588d9a7d125d040ec1689c3d1c155b2c298
scarf <greenscarf005@gmail.com>
2022-06-14 23:13:27 +0900

    refactor: 함수로 좀 더 나누기

--- src/util/strutil/format.cpp
+++ src/util/strutil/format.cpp
@@ -16,38 +16,43 @@ struct FormatState {
       : fmt(fmt), args(args), arg_counter(0) {}
 
   // 멤버 함수
-  bool on_escape(size_t i, const string& match) {
+  bool on_escaped_bracket(size_t i, const string& match) {
     return fmt.substr(i, 2) == match;
   }
+
   void advance_and_append(size_t& i, const string& to_add) {
     i++;
     result += to_add;
+  }
+
+  void advance_to_closing_bracket(size_t& i) {
+    result += args.at(arg_counter++);
+    size_t j = i + 1;
+    for (; j < fmt.size(); j++) {
+      if (fmt[j] == '{') {
+        throw std::runtime_error("unmatched opening brace at " +
+                                 util::to_string(j));
+      } else if (fmt[j] == '}') {
+        i = j;
+        break;
+      }
+    }
+    if (j == fmt.size()) {
+      throw std::runtime_error(
+          "missing closing brace '}' at end of format string");
+    }
   }
 };
 
 string format(const string& fmt, const vector<string>& args) {
   FormatState state(fmt, args);
-  size_t i = 0;
-  for (; i < fmt.size(); i++) {
-    if (state.on_escape(i, "{{")) {
+
+  for (size_t i = 0; i < fmt.size(); i++) {
+    if (state.on_escaped_bracket(i, "{{")) {
       state.advance_and_append(i, "{");
     } else if (fmt[i] == '{') {
-      state.result += args.at(state.arg_counter++);
-      size_t j = i + 1;
-      for (; j < fmt.size(); j++) {
-        if (fmt[j] == '{') {
-          throw std::runtime_error("unmatched opening brace at " +
-                                   util::to_string(j));
-        } else if (fmt[j] == '}') {
-          i = j;
-          break;
-        }
-      }
-      if (j == fmt.size()) {
-        throw std::runtime_error(
-            "missing closing brace '}' at end of format string");
-      }
-    } else if (state.on_escape(i, "}}")) {
+      state.advance_to_closing_bracket(i);
+    } else if (state.on_escaped_bracket(i, "}}")) {
       state.advance_and_append(i, "}");
     } else if (fmt[i] == '}') {
       throw std::runtime_error("unmatched closing brace at " +
