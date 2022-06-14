a0a1b46928e17a66723b2b5b1005ca7cc6641ead
scarf <greenscarf005@gmail.com>
2022-06-14 20:29:02 +0900

    refactor: 조?금 함수로 빼기

--- src/util/strutil/format.cpp
+++ src/util/strutil/format.cpp
@@ -11,17 +11,26 @@ struct FormatState {
   std::string result;
   int arg_counter;
 
+  // 생성자
   FormatState(const string& fmt, const vector<string>& args)
       : fmt(fmt), args(args), arg_counter(0) {}
+
+  // 멤버 함수
+  bool on_escape(size_t i, const string& match) {
+    return fmt.substr(i, i + 1) == match;
+  }
+  void advance_and_append(size_t& i, const string& to_add) {
+    i++;
+    result += to_add;
+  }
 };
 
 string format(const string& fmt, const vector<string>& args) {
   FormatState state(fmt, args);
 
   for (size_t i = 0; i < fmt.size(); i++) {
-    if (fmt.substr(i, i + 1) == "{{") {
-      state.result += '{';
-      i++;
+    if (state.on_escape(i, "{{")) {
+      state.advance_and_append(i, "{");
     } else if (fmt[i] == '{') {
       state.result += args.at(state.arg_counter++);
       size_t j = i + 1;
@@ -38,9 +47,8 @@ string format(const string& fmt, const vector<string>& args) {
         throw std::runtime_error(
             "missing closing brace '}' at end of format string");
       }
-    } else if (fmt.substr(i, i + 1) == "}}") {
-      state.result += '}';
-      i++;
+    } else if (state.on_escape(i, "}}")) {
+      state.advance_and_append(i, "}");
     } else if (fmt[i] == '}') {
       throw std::runtime_error("unmatched closing brace at " +
                                util::to_string(i));
