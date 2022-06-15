b62b94ff5557ee2ed73a90863d752b2575a7860c
scarf <greenscarf005@gmail.com>
2022-06-15 09:33:15 +0900

    refactor: 클래스 안으로 구현부 옮기기

--- src/util/strutil/format.cpp
+++ src/util/strutil/format.cpp
@@ -5,63 +5,65 @@ namespace util {
 using std::string;
 using std::vector;
 
-struct FormatState {
+class FormatState {
+ private:
   string fmt;
   vector<string> args;
   std::string result;
-  int arg_counter;
+  int arg_index;
+  size_t curs;
 
+ public:
   // 생성자
   FormatState(const string& fmt, const vector<string>& args)
-      : fmt(fmt), args(args), arg_counter(0) {}
-
-  // 멤버 함수
-  bool on_escaped_bracket(size_t i, const string& match) {
-    return fmt.substr(i, 2) == match;
+      : fmt(fmt), args(args), arg_index(0), curs(0) {
+    for (; curs < fmt.size(); curs++) {
+      if (on_escaped_bracket("{{")) {
+        advance_and_append("{");
+      } else if (on_char('{')) {
+        advance_to_closing_bracket();
+      } else if (on_escaped_bracket("}}")) {
+        advance_and_append("}");
+      } else if (on_char('}')) {
+        throw std::runtime_error("unmatched closing brace at " +
+                                 util::to_string(curs));
+      } else {
+        result += fmt[curs];
+      }
+    }
   }
 
-  void advance_and_append(size_t& i, const string& to_add) {
-    i++;
+  // 멤버 함수
+  bool on_char(const char c) { return (fmt[curs] == c); }
+
+  bool on_escaped_bracket(const string& match) {
+    return fmt.substr(curs, 2) == match;
+  }
+
+  void advance_and_append(const string& to_add) {
+    curs++;
     result += to_add;
   }
 
-  void advance_to_closing_bracket(size_t& i) {
-    result += args.at(arg_counter++);
-    size_t j = i + 1;
-    for (; j < fmt.size(); j++) {
-      if (fmt[j] == '{') {
-        throw std::runtime_error("unmatched opening brace at " +
-                                 util::to_string(j));
-      } else if (fmt[j] == '}') {
-        i = j;
-        break;
+  void advance_to_closing_bracket() {
+    result += args.at(arg_index++);
+    for (size_t i = curs + 1; i < fmt.size(); i++) {
+      if (fmt[i] == '{') {
+        throw std::runtime_error("unmatched opening brace at " + to_string(i));
+      } else if (fmt[i] == '}') {
+        curs = i;
+        return;
       }
     }
-    if (j == fmt.size()) {
-      throw std::runtime_error(
-          "missing closing brace '}' at end of format string");
-    }
+    throw std::runtime_error(
+        "missing closing brace '}' at end of format string");
   }
+
+  operator string() { return result; }
 };
 
 string format(const string& fmt, const vector<string>& args) {
-  FormatState state(fmt, args);
-
-  for (size_t i = 0; i < fmt.size(); i++) {
-    if (state.on_escaped_bracket(i, "{{")) {
-      state.advance_and_append(i, "{");
-    } else if (fmt[i] == '{') {
-      state.advance_to_closing_bracket(i);
-    } else if (state.on_escaped_bracket(i, "}}")) {
-      state.advance_and_append(i, "}");
-    } else if (fmt[i] == '}') {
-      throw std::runtime_error("unmatched closing brace at " +
-                               util::to_string(i));
-    } else {
-      state.result += fmt[i];
-    }
-  }
-  return state.result;
+  return FormatState(fmt, args);
 }
 
 }  // namespace util
