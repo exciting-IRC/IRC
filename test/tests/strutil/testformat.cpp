8ae78d6413c54cd55c517644efc0cf794fc8b6ea
scarf <greenscarf005@gmail.com>
2022-06-15 10:51:40 +0900

    test: 테스트 추가

---
+++ test/tests/strutil/testformat.cpp
@@ -0,0 +1,47 @@
+#include <gtest/gtest.h>
+#include <rapidcheck.h>
+#include <rapidcheck/gtest.h>
+
+#include <stack>
+#include <util/strutil/strutil.hpp>
+
+using strvec = std::vector<std::string>;
+
+TEST(FormatTest, edgeCases) {
+  RC_ASSERT(FMT("{foo} {} {{}}", ("hello", 3)).str() == "hello 3 {}");
+  for (auto fmt : strvec{"{", "}", "{}"})
+    RC_ASSERT_THROWS_AS(FMT(fmt, ()), std::invalid_argument);
+}
+
+auto create_fmtbrace() -> std::string {
+  const std::string candidate = *rc::gen::arbitrary<std::string>();
+
+  if (candidate.find("{") != std::string::npos or
+      candidate.find("}") != std::string::npos)
+    return "{}";
+
+  return "{" + candidate + "}";
+}
+
+RC_GTEST_PROP(FormatTest, general, (const strvec& v)) {
+  RC_PRE(std::none_of(v.begin(), v.end(), [](const std::string& s) {
+    return s.find("{") != std::string::npos;
+  }));
+  RC_PRE(std::none_of(v.begin(), v.end(), [](const std::string& s) {
+    return s.find("}") != std::string::npos;
+  }));
+
+  std::string result = [&]() {
+    std::string s;
+    for (auto e : v)
+      s += e + e;
+    return s;
+  }();
+  std::string fmt = [&]() {
+    std::string s;
+    for (auto e : v)
+      s += e + create_fmtbrace();
+    return s;
+  }();
+  ASSERT_EQ(util::format(fmt, v).str(), result);
+}
