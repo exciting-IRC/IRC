e75acf35d2425ca53c106b0d834dcc4dea8f573a
scarf <greenscarf005@gmail.com>
2022-06-16 13:14:02 +0900

    fix: 변수는 src에

--- src/command/returnformat.hpp
+++ src/command/returnformat.hpp
@@ -10,22 +10,10 @@
 namespace util {
 using std::pair;
 using std::string;
-using util::p;
 
 typedef ConstMap<returnCode, string> return_map;
 typedef pair<returnCode, string> return_pair;
 
-return_map retfmt = container_of<return_map, return_pair>(
-    p(RPL_WELCOME,
-      "Welcome to the Internet Relay Network {nick}!{user}@{host}"),
-    p(RPL_YOURHOST, "Your host is {servername}, running version {ver}"),
-    p(RPL_CREATED, "This server was created {date}"),
-    p(RPL_MYINFO,
-      "{servername} {version} {available user modes} {available channel "
-      "modes}"),
-    p(ERR_NONICKNAMEGIVEN, ":No nickname given"),
-    p(ERR_NOSUCHNICK, "{nick} :No such nick/channel"),
-    p(ERR_ERRONEUSNICKNAME, "{nick} :Erroneous nickname"));
+extern const return_map retfmt;
 }  // namespace util
-
 #endif  // COMMAND_REPLIES_HPP
