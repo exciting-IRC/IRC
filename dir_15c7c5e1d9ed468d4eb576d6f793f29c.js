1df832de11dda04850e18e238f8843163e499480
scarf005 <scarf005@users.noreply.github.com>
2022-06-20 02:11:13 +0000

    deploy: 3b07a92c27999227373a715a6133e88c8606271d

--- dir_15c7c5e1d9ed468d4eb576d6f793f29c.js
+++ dir_15c7c5e1d9ed468d4eb576d6f793f29c.js
@@ -1,4 +1,5 @@
 var dir_15c7c5e1d9ed468d4eb576d6f793f29c =
 [
-    [ "algorithm.hpp", "algorithm_8hpp_source.html", null ]
+    [ "algorithm.hpp", "algorithm_8hpp_source.html", null ],
+    [ "functor.hpp", "functor_8hpp_source.html", null ]
 ];
