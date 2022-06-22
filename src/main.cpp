0c93f9063362294736bef8635fe5b5198006bbb5
cjeon <cjeon@student.42seoul.kr>
2022-06-22 15:05:51 +0900

    바인드 할 주소를 0.0.0.0으로 변경

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@
 #include "util/FixedBuffer/FixedBuffer.hpp"
 #include "util/vargs/container_of.hpp"
 
-const static char *bind_addr = "127.0.0.1";
+const static char *bind_addr = "0.0.0.0";
 const static int port = 6667;
 
 static sig_atomic_t recived_sig;
