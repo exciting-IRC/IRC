a7fd95974f9cab5dfa49359e5d427336cf9d4f8c
scarf <greenscarf005@gmail.com>
2022-06-22 17:28:29 +0900

    docs: 주석 추가

--- src/util/general/logging.hpp
+++ src/util/general/logging.hpp
@@ -9,10 +9,13 @@ namespace util {
 using std::string;
 using std::vector;
 
+/// 타임스탬프와 오류 여부, 보내고 싶은 메시지를 stdout에 출력하기
 void debug(const string& msg, bool ok = true);
 
+/// 메시지를 받았을 때
 void debug_input(const string& command, bool ok = true);
 
+/// 메시지를 보낼 때
 void debug_output(const string& payload, bool ok = true);
 
 }  // namespace util
