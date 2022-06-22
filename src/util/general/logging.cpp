d80cfffc51ec326fcf4ead7c21803e238d7500e9
scarf <greenscarf005@gmail.com>
2022-06-22 17:24:41 +0900

    refactor: 색상도 인자로 바꾸기

--- src/util/general/logging.cpp
+++ src/util/general/logging.cpp
@@ -10,15 +10,15 @@ namespace util {
 void debug(const string& msg, bool ok) {
   static const array<string, 2> result = {BHRED " ERR!", ""};
 
-  COUT_FMT("{time}{result} {msg}" END,
-           (get_current_time("[%H:%M:%S]"), result[ok], msg));
+  COUT_FMT("{time}{result} {msg}{}",
+           (get_current_time("[%H:%M:%S]"), result[ok], msg, END));
 }
 
 void debug_input(const string& command, bool ok) {
-  debug(FMT(BHCYN "-> " HRED "\"{}\"", (command)), ok);
+  debug(FMT(BHCYN "-> {red}\"{}\"", (HRED, command)), ok);
 }
 
 void debug_output(const string& payload, bool ok) {
-  debug(FMT(BHGRN "<- " HBLU "\"{}\"", (payload)), ok);
+  debug(FMT(BHGRN "<- {blue}\"{}\"", (HBLU, payload)), ok);
 }
 }  // namespace util
