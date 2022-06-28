440a5502df966ae805f519dbd6fe3ac39b189edd
scarf <greenscarf005@gmail.com>
2022-06-28 11:32:19 +0900

    feat: bot용 config.yml

--- bot/main.cpp
+++ bot/main.cpp
@@ -2,16 +2,11 @@
 #include "MineSweeper.hpp"
 
 int main() {
-  BotConfig config;
-
-  config.password = "password";
-  config.port = 6667;
-  config.server_ip = "127.0.0.1";
-
+  BotConfig config = BotConfig::from("config.yml");
   Bot bot(config);
 
   bot.init(1024);
 
-  while (true)
+
     bot.loop();
 }
